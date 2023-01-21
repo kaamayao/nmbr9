b9ead25e1ab4f59ff90ea1bc596bff63572468da
kaamayao <kaamayao@unal.edu.co>
2023-01-21 02:29:15 -0500

    Adds rotation

--- scenes/Tokens.gd
+++ scenes/Tokens.gd
@@ -9,6 +9,8 @@ var gravity = -60
 var token_collision = null
 var current_token = null
 var grid_movement_velocity = 2
+var token_rotation = 0
+var current_rotation = 0
 
 func _ready():
 	randomize()
@@ -20,6 +22,7 @@ func _ready():
 func _physics_process(delta):
 	if current_token != null:
 		velocity = Vector3()
+		token_rotation = 0
 		get_input()
 		move_by_gravity(delta)
 
@@ -37,7 +40,17 @@ func move_by_gravity(delta):
 		token_collision = current_token.move_and_collide(velocity * delta)
 		if token_collision != null:
 			select_next_token()
-		
+
+func rotate_token(direction):
+	current_rotation = int(fposmod(current_rotation + (direction * 90), 360))
+	token_rotation = direction * 1.5708
+	if current_rotation == 90 or current_rotation == 180:
+		velocity.x = 1
+		velocity.z = 1
+	if current_rotation == 270 or current_rotation == 0:
+		velocity.x = -1
+		velocity.z = -1
+
 func get_input():
 	if Input.is_action_just_pressed("ui_up"):
 		velocity.x = grid_movement_velocity
@@ -47,6 +60,11 @@ func get_input():
 		velocity.z = grid_movement_velocity
 	if Input.is_action_just_pressed("ui_left"):
 		velocity.z = -grid_movement_velocity
+	if Input.is_action_just_pressed("ui_a"):
+		rotate_token(1)
+	if Input.is_action_just_pressed("ui_b"):
+		rotate_token(-1)
 	if Input.is_action_just_pressed("ui_select"):
 		gravity_is_active = true
 	current_token.move_and_collide(velocity)
+	current_token.rotate_y(token_rotation)
